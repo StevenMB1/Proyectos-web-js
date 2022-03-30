a317b9923508387ba15aada5ed334e06746eaf13
Steven Medina <74547441+StevenMB1@users.noreply.github.com>
2022-03-30 10:25:22 -0400

    first commit

---
+++ Aplicacion-color-picker/practica-dom1.js
@@ -0,0 +1,19 @@
+const inputColor = document.querySelector('#inputColor')
+const btnVisualizar = document.querySelector('#btnVisualizar')
+const parrafoExa = document.querySelector('#parrafoExa')
+const cardColor = document.querySelector('#cardColor')
+
+console.log(inputColor.value)
+
+btnVisualizar.addEventListener('click', () => {
+
+    console.log('me diste click')
+    console.log(inputColor.value)
+    parrafoExa.textContent = inputColor.value
+    cardColor.style.backgroundColor = inputColor.value
+    
+    navigator.clipboard
+        .writeText(inputColor.value)
+        .then( () => console.log('texto copiado'))
+        .catch( (e) => console.log(e))
+})
